3d4b1c5af4db0779e352473f8321694ceb216b2c
Hunter <utsavwagle508@gmail.com>
2024-06-03 17:33:37 +0545

    js variable and data type

---
+++ Day 3/README.md
@@ -0,0 +1,11 @@
+# 🌟 Day 3 of #60DaysOfLearning2024! 🚀
+
+## Today, I started learning JavaScript (JS), a powerful scripting language for web development:
+
+- Using the console
+- Understanding data types
+- Variables and operators
+- Assignment operators
+- Difference between null and undefined
+
+Excited to dive deeper into JavaScript! #LSPPDay3 #LearningWithLeapfrog
---
+++ Day 3/variable.js
@@ -0,0 +1,16 @@
+let num = 99; // number
+let name = "utsav wagle"; // string
+const pi = 3.14; // const number
+
+// printing the data type of the variable 
+console.log("the datatype of the num is ", typeof num);
+console.log("the datatype of the name is ", typeof name);
+console.log("the datatype of the pi is ", typeof pi);
+
+// null and undefined example 
+let numExample = null;
+let undefinedExample;
+
+// printing the data type of the variable 
+console.log("the datatype of the numExample is ", typeof numExample);
+console.log("the datatype of the undefinedExample is ", typeof undefinedExample);
