1d1da320878aa2e798b89b4fda83d3784967b109
Hunter <utsavwagle508@gmail.com>
2024-06-03 17:48:36 +0545

    arithmetic operator in js

---
+++ Day 3/arithmeticOperator.js
@@ -0,0 +1,30 @@
+let num1 = 10;
+let num2 = 100;
+
+// arithmetic operator 
+
+//addition operator
+console.log("the sum of num1 and num2 is ", num1 + num2);
+
+//subtraction operator
+console.log("the difference of num2 and num1 is ", num2 - num2);
+
+// multiplication operator
+console.log("the Multiplication of num1 and num2 is ", num2 * num2);
+
+// power/Exponentiation opeator
+console.log("2 power of num1 is ", num1 ** 2);
+
+//modulus operator // remainder 
+
+console.log("the remainder is ", num2 % num1);
+
+// increment and decrement operator 
+console.log("after the increment the value of num1 is ", num1++)
+
+console.log("after the decrement the value of num1 is ", num1--)
+
+
+
+
+
