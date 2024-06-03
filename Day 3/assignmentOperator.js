356b8e866fc2c925ea6610600dbaf7192b198c49
Hunter <utsavwagle508@gmail.com>
2024-06-03 17:55:25 +0545

    assignment operator in js

---
+++ Day 3/assignmentOperator.js
@@ -0,0 +1,14 @@
+let x_axis = 20;
+let y_axis = 10;
+
+// assignment operator 
+console.log("the value of x_axis after adding 10 is ", x_axis += 10);
+
+console.log("the value of x_axis after subtracting 5 is ", x_axis -= 5);
+
+console.log("the value of y_axis after multiplying by 2 is ", y_axis *= 2);
+
+console.log("the value of y_axis after dividing by 2 is ", y_axis /= 2);
+
+
+
