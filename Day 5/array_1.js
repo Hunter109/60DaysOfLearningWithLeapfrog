3eb4004b76bb2252c7f2cb288f91819ad511bc90
Hunter <utsavwagle508@gmail.com>
2024-06-05 18:39:02 +0545

    array in js

---
+++ Day 5/array_1.js
@@ -0,0 +1,16 @@
+// array in javascript
+let university = ["pokhara", "kathmandu", "Tribhuvan"]
+// acessing the element of the array 
+console.log(university[0]);
+console.log(university[2]);
+
+// in javascript we can create mix array which can contain number ,stirng etc 
+let info = ["utsav wagle", 21, "bharatpur-7", 1060.77];
+console.log(info[0]);
+console.log(info[info.length - 1]);
+
+// array are mutable in js 
+info[0] = "hunter";
+console.log(info);
+
+
