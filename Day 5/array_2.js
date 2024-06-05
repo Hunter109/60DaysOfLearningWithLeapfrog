6e5c23326e465e4934a669f30284c29ef32bdb43
Hunter <utsavwagle508@gmail.com>
2024-06-05 18:58:55 +0545

    push pop method in array

---
+++ Day 5/array_2.js
@@ -0,0 +1,12 @@
+// array method in javascript 
+
+let carCompany = ["volvo", "toyota", "BMW", "Honda"];
+//push method is use to push the data in the end of the array
+carCompany.push("Audi");
+carCompany.push("BYD");
+carCompany.push("Jeep");
+console.log(carCompany);
+
+//pop method is use to pop the last element of the array
+carCompany.pop();
+console.log(carCompany)
