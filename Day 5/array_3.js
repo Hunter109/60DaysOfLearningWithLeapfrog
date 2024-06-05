71dcefbaeaacf3fd4fc855dfa5a2727ebdb3ae3b
Hunter <utsavwagle508@gmail.com>
2024-06-05 19:06:45 +0545

    shift and unshift method in array

---
+++ Day 5/array_3.js
@@ -0,0 +1,18 @@
+// shift unshift method in array 
+
+// unshift() method is use to add element at the start of the array 
+let bike = ["honda", "beneli", "ktm", "pulsar"];
+bike.unshift("mt-15");
+bike.unshift("crossfire");
+console.log(bike);
+console.log(`the length of the array after unshift is ${bike.length}`)
+
+
+//shift() method is use to remove element at the start of the array 
+bike.shift();
+console.log(bike)
+console.log(`the length of the array after shift  is ${bike.length}`)
+
+
+
+
