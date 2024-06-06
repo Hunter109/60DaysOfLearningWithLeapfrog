1a0e1f4d9ff429770417bd83f6f4405c9ba94d2e
Hunter <utsavwagle508@gmail.com>
2024-06-06 21:29:11 +0545

    day 6 image

--- Day 6/forLoop.js
+++ Day 6/forLoop.js
@@ -1,15 +1,16 @@
-// loop is use to perform repetative process 
+// loop is use to perform repetative process
 
 let arr = [10, 30, 40, 50, 60, 78];
 console.log(arr.length);
 console.log("printing all teh data of the array using for loop");
+
 for (let i = 0; i <= arr.length - 1; i++) {
     console.log(arr[i]);
 }
 console.log("\n");
 let city = ["chitwan", "kathmandu", "pokhara", "hetauda", "butwal"];
 console.log("printing all the data of city array using for loop");
+
 for (let index = 0; index < city.length; index++) {
     console.log(city[index]);
-
 }
