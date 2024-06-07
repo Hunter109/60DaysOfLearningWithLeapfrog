85118436aa05c410ebd55334c0a84d21a613a040
Hunter <utsavwagle508@gmail.com>
2024-06-07 20:15:44 +0545

    array of object

---
+++ Day 7/arrayObject.js
@@ -0,0 +1,25 @@
+// object inside array which is also called as array of object
+let student = [
+    {
+        name: "soviyat",
+        program: "BE Civil",
+        batch: "6th"
+    }, {
+        name: "sandesh",
+        program: "BE Civil",
+        batch: "5th"
+    }
+];
+// accesing the key value pair in array of object using index and dot operator
+console.log(student[0].name);
+console.log(student[0].program);
+
+console.log(student[1].name);
+console.log(student[1].program);
+
+//output of the program
+//soviyat
+//BE Civil
+//sandesh
+//BE Civil
+
