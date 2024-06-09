bf759c711c9c9e08d0f4e4e606ac82f5ada30b37
Hunter <utsavwagle508@gmail.com>
2024-06-09 18:56:55 +0545

    set timeout function

---
+++ Day 9/setTimeout.js
@@ -0,0 +1,9 @@
+// Using setTimeout to call  after 2000 milliseconds (2 seconds)
+setTimeout(() => {
+    console.log(
+        "This message will print 2 seconds after the execution of the program."
+    );
+}, 2000);
+
+// Additional code to demonstrate program flow
+console.log("This message will print immediately.");
