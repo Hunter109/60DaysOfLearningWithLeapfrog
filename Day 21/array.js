9952094904fc19cc14a12a7d9120a7c33b9d1fae
Hunter <utsavwagle508@gmail.com>
2024-06-21 21:55:10 +0545

    revised

---
+++ Day 21/array.js
@@ -0,0 +1,13 @@
+// filter array method 
+function filterNumber(arr) {
+    return arr.filter(num => num > 10);
+}
+console.log(filterNumber([5, 12, 8, 130, 44]));
+
+
+// map array method 
+
+function squareNumber(arr) {
+    return arr.map(num => num * num);
+}
+console.log(squareNumber([1, 2, 3, 4])); 
