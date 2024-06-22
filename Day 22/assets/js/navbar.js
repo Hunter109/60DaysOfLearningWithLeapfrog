002f0f3cf47c309ac32a5c652b8eb53ced23ca7e
Hunter <utsavwagle508@gmail.com>
2024-06-22 17:18:25 +0545

    updated css and js

--- Day 22/assets/js/navbar.js
+++ Day 22/assets/js/navbar.js
@@ -1,16 +1,21 @@
- // Navbar Toogle
- const toggleBtn = document.querySelector(".toggle_btn");
- const toggleBtnIcon = document.querySelector(".toggle_btn i");
- const dropDownMenu = document.querySelector(".dropdown_menu");
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
 
- toggleBtn.onclick = function () {
-     dropDownMenu.classList.toggle("open");
-     const isOpen = dropDownMenu.classList.contains("open");
-     toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
- };
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle("open");
+    const isOpen = dropDownMenu.classList.contains("open");
+    if (isOpen) {
+        toggleBtnIcon.classList.remove("fa-bars");
+        toggleBtnIcon.classList.add("fa-xmark");
+    } else {
+        toggleBtnIcon.classList.remove("fa-xmark");
+        toggleBtnIcon.classList.add("fa-bars");
+    }
+};
 
 
- function redirectToNextPage() {
+function redirectToNextPage() {
     window.location.href = "./allcourse.html";
 }
 
