aebd89608715f26515f2e532c7290ed2e3aa6f23
Hunter <utsavwagle508@gmail.com>
2024-06-29 10:21:15 +0545

    60 day learning histroy tracker

---
+++ Day 29/script.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 7; i++) {
+    let showContent = document.getElementById(`showContent${i}`);
+    let btn = document.getElementById(`day${i}`);
+
+    btn.addEventListener("click", () => {
+        alert("click");
+        setTimeout(() => {
+            showContent.classList.toggle("hidden");
+            if (showContent.classList.contains("hidden")) {
+                btn.innerText = "View Details";
+            } else {
+                btn.innerText = "Close Details";
+            }
+        }, 300);
+    });
+}
+
+
