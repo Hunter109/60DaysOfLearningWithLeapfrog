07cf0e5c51dea889c4f00e979a49dc33f79b4ba8
Hunter <utsavwagle508@gmail.com>
2024-07-08 22:50:36 +0545

    image slider

---
+++ Day 38/script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+        delay: 5000,
+    },
+});
