5968855a3889b003d1b0293243c2130614d28ed2
Hunter <utsavwagle508@gmail.com>
2024-07-21 12:59:47 +0545

    Day 51

--- Day 51/script.js
+++ Day 51/script.js
@@ -15,3 +15,32 @@ function calculate() {
         document.getElementById('display').value = 'Error';
     }
 }
+
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const starsContainer = document.querySelector('.stars-container');
+    for (let i = 0; i < 500; i++) {
+        const star = document.createElement('div');
+        star.className = 'stars';
+        star.style.top = Math.random() * 100 + 'vh';
+        star.style.left = Math.random() * 100 + 'vw';
+        star.style.animationDuration = Math.random() * 3 + 2 + 's';
+        starsContainer.appendChild(star);
+    }
+});
